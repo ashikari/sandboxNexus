36720d2e6ee53d0ae951fbb7aa07579c1c5dedf0
ashikari <ashikari123@gmail.com>
2018-01-23 19:28:34 -0500

    scripts to src and small bug fixes

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/ashikari/sandboxNexus/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ashikari/sandboxNexus/devel;/opt/ros/kinetic".split(';') if "/home/ashikari/sandboxNexus/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/ashikari/TSERA/devel;/home/ashikari/sandboxNexus/devel;/opt/ros/kinetic".split(';') if "/home/ashikari/TSERA/devel;/home/ashikari/sandboxNexus/devel;/opt/ros/kinetic" != "" else []
