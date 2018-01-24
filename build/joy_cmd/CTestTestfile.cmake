0c55f7f024514084b84b051d3d56798ef7c6ba80
Filippos Sotiropoulos <filippos.sotiropoulos@gmail.com>
2018-01-23 20:30:10 -0500

    build devel removed

--- build/joy_cmd/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashikari/sandboxNexus/src/joy_cmd
-# Build directory: /home/ashikari/sandboxNexus/build/joy_cmd
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/kinematics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashikari/sandboxNexus/src/kinematics
-# Build directory: /home/ashikari/sandboxNexus/build/kinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
